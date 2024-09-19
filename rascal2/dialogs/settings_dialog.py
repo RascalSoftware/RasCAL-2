969b430023676589c2ca437cb66fdff37db40d2c
PaulSharp <44529197+DrPaulSharp@users.noreply.github.com>
2024-09-19 10:32:36 +0100

    Connects settings dialog to program settings

--- rascal2/dialogs/settings_dialog.py
+++ rascal2/dialogs/settings_dialog.py
@@ -1,5 +1,3 @@
-from enum import StrEnum, unique
-
 from PyQt6 import QtCore, QtWidgets
 
 from rascal2.core.settings import Settings
@@ -27,21 +25,21 @@ class SettingsDialog(QtWidgets.QDialog):
         super().__init__(parent)
 
         self.setModal(True)
-        self.setMinimumWidth(700)
+        self.setMinimumWidth(400)
 
-        current_settings = self.parent().settings
+        self.current_settings = parent.settings.copy()
 
         tab_widget = QtWidgets.QTabWidget()
-        tab_widget.addTab(GeneralSettings(self, current_settings), "General")
-        tab_widget.addTab(LoggingSettings(self, current_settings), "Logging")
-        tab_widget.addTab(WindowsSettings(self, current_settings), "Windows")
+        tab_widget.addTab(GeneralSettings(self, self.current_settings), "General")
+        tab_widget.addTab(LoggingSettings(self, self.current_settings), "Logging")
+        tab_widget.addTab(WindowsSettings(self, self.current_settings), "Windows")
 
         button_box = QtWidgets.QDialogButtonBox(
             QtWidgets.QDialogButtonBox.StandardButton.Ok | QtWidgets.QDialogButtonBox.StandardButton.Cancel
         )
 
-        button_box.accepted.connect(self.accept)
-        button_box.rejected.connect(self.reject)
+        button_box.accepted.connect(self.update_settings)
+        button_box.rejected.connect(self.reject_settings)
 
         main_layout = QtWidgets.QVBoxLayout()
         main_layout.addWidget(tab_widget)
@@ -49,25 +47,52 @@ class SettingsDialog(QtWidgets.QDialog):
         self.setLayout(main_layout)
         self.setWindowTitle("Settings")
 
+    def update_settings(self):
+        """Accept the changed settings"""
+        self.parent().settings = self.current_settings.copy()
+        print(self.current_settings)
+        print(self.parent().settings)
+        self.accept()
+
+    def reject_settings(self):
+        """Accept the changed settings"""
+        print(self.current_settings)
+        print(self.parent().settings)
+        self.reject()
+
 
 class SettingsTab(QtWidgets.QWidget):
     def __init__(self, parent: QtWidgets.QWidget, current_settings: Settings, group: str):
         super().__init__(parent)
 
+        self.settings = current_settings
         tab_layout = QtWidgets.QGridLayout()
+
+        self.widgets = {}
+        self.slots = {}
 
         for i, setting in enumerate(group_settings[group]):
             label_text = setting.replace("_", " ")
             tab_layout.addWidget(QtWidgets.QLabel(label_text), i, 0)
-            setting_widget = ValidatedInputWidget(getattr(current_settings, "model_fields")[setting])
+            self.widgets[setting] = ValidatedInputWidget(getattr(current_settings, "model_fields")[setting])
             try:
-                setting_widget.set_data(getattr(current_settings, setting))
+                self.widgets[setting].set_data(getattr(self.settings, setting))
             except TypeError:
-                setting_widget.set_data(str(getattr(current_settings, setting)))
-            tab_layout.addWidget(setting_widget, i, 1)
+                self.widgets[setting].set_data(str(getattr(self.settings, setting)))
+            self.slots[setting] = self.create_slot(setting)
+            self.widgets[setting].edited_signal.connect(self.slots[setting])
+            tab_layout.addWidget(self.widgets[setting], i, 1)
 
         tab_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
         self.setLayout(tab_layout)
+
+    def create_slot(self, setting):
+        """Returns a slot that updates the settings to connect to the edited_signal of the given widget."""
+
+        def modify_settings():
+            setattr(self.settings, setting, self.widgets[setting].get_data())
+
+        return modify_settings
 
 
 class GeneralSettings(SettingsTab):
