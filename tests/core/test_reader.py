0eb774997b1720879d36d4a61427689bbea81997
alexhroom <alex.room@btinternet.com>
2025-04-28 09:51:15 +0100

    fixed tests

--- tests/core/test_reader.py
+++ tests/core/test_reader.py
@@ -19,21 +19,21 @@ DATA_PATH = Path(__file__, "../../data/").resolve()
 def test_read_csv(filename, expected_size):
     """Test the plaintext reader successfully reads in data."""
     data = readers.TextDataReader().read(DATA_PATH / filename)
-    assert data[0].data.shape == expected_size
+    assert next(data).data.shape == expected_size
 
 
 @pytest.mark.parametrize("filename, expected_size", (["f88904_06.asc", (93, 3)],))
 def test_read_asc(filename, expected_size):
     """Test the .asc reader successfully reads in data."""
     data = readers.AscDataReader().read(DATA_PATH / filename)
-    assert data[0].data.shape == expected_size
+    assert next(data).data.shape == expected_size
 
 
 @pytest.mark.parametrize("filename, expected_size", (["INTER_61440_IvsQ_binned.nxs", (134, 3)],))
 def test_read_nxs(filename, expected_size):
     """Test the .nxs reader successfully reads in data."""
     data = readers.NexusDataReader().read(DATA_PATH / filename)
-    assert data[0].data.shape == expected_size
+    assert next(data).data.shape == expected_size
 
 
 @pytest.mark.parametrize(
@@ -46,5 +46,6 @@ def test_read_nxs(filename, expected_size):
 def test_read_ort(filename, expected_name, expected_size):
     """Test the .ort reader successfully reads in data."""
     data = readers.OrtDataReader().read(DATA_PATH / filename)
-    assert data[0].name == expected_name
-    assert data[0].data.shape == expected_size
+    dataset = next(data)
+    assert dataset.name == expected_name
+    assert dataset.data.shape == expected_size
