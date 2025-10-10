c0ee4bdc1e5794cc80d17bf28fcfaf8ed93911a3
abuts <Alex.Buts@stfc.ac.uk>
2025-10-10 19:43:59 +0100

    Re #116 Small changes, may be not necessary

--- rascal2/dialogs/about_dialog.py
+++ rascal2/dialogs/about_dialog.py
@@ -1,47 +1,21 @@
 from PyQt6 import QtGui, QtWidgets, QtCore
 import logging as log_class
 
-
+import rascal2
 from rascal2.config import MATLAB_HELPER, STATIC_PATH, path_for
 
 class AboutDialog(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
         # Define internal variables
-        self._version = "1.0.0"
-        self._Matlab_path = "None"
-        self._Log_File = "None"
-        self._Working_dir = "None"
 
-        # Main header. Format information about Rascal 2
-        Info_template = """
-               <b><i><span style="font-family:Georgia; font-size:42pt;">RasCAL 2</b></span><br>
-               <span style="font-family:Georgia; "font-size=38pt;">            
-                A GUI for Reflectivity Algorithm Toolbox<br><br>
-                 <table style="text-align:left;">
-                     <tr>
-                       <td>Version:</td><td>{}</td>
-                       </tr><tr>
-                       <td>Matlab Path:</td><td>{}</td>
-                       </tr><tr>
-                       <td>Log File:</td><td>{}</td>
-                     </tr> 
-                 </table><br><br>     
-                </span>
-                <span style="font-family:Georgia; "font-size=32pt;">
-                Distributed under the BSD 3-Clause License<br>
-                <p>Copyright &copy; 2018-2025 ISIS Neutron and Muon Source.</p>
-                All rights reserved
-                </span></i>
-                """
         # Define main window
         self.setWindowTitle("About RasCAL 2")
         self.setMinimumWidth(500)
         self.setMinimumHeight(250)
 
-        label_text = Info_template.format(self._version,self._Matlab_path,self._Log_File)
-        rascal_label = QtWidgets.QLabel(label_text)
-        rascal_label.setWordWrap(True)
+        self._rascal_label = QtWidgets.QLabel("RASCAL-2")
+        self._rascal_label.setWordWrap(True)
 
         # Load RASCAL logo from appropriate image
         logo_label = QtWidgets.QLabel()
@@ -61,7 +35,7 @@ class AboutDialog(QtWidgets.QDialog):
         left_layout.addWidget(logo_label,alignment=QtCore.Qt.AlignmentFlag.AlignTop)
         #
         right_layout = QtWidgets.QVBoxLayout() # place for text
-        right_layout.addWidget(rascal_label)
+        right_layout.addWidget(self._rascal_label)
         # arrange logo and text into appropriate ratio
         row1_layout.addLayout(left_layout,stretch=1)
         row1_layout.addLayout(right_layout,stretch=4)
@@ -82,12 +56,42 @@ class AboutDialog(QtWidgets.QDialog):
         retrieved from general class information
         """
         #self._Working_dir = parent.settings.get("Working_dir")
-        self._Matlab_path = MATLAB_HELPER.get_matlab_path()
-        if not self._Matlab_path:
-            self._Matlab_path = "None"
+        matlab_path = MATLAB_HELPER.get_matlab_path()
+        if not matlab_path:
+            matlab_path = "None"
+
+        log_file = "None"
         logger = parent.logging
         for h in logger.handlers:
             if isinstance(h, log_class.FileHandler):
-                self._Log_File = h.baseFilename
+                log_file = h.baseFilename
             else:
-                self._Log_File = "None"
+                log_file = "None"
+
+        Working_dir = "None"
+        log_level  = "INFO"
+        # Main header. Format information about Rascal 2
+        info_template = """
+               <b><i><span style="font-family:Georgia; font-size:42pt;">RasCAL 2</b></span><br><br>
+               <span style="font-family:Georgia; "font-size=34pt;">            
+                A GUI for Reflectivity Algorithm Toolbox<br><br>
+                 <table style="text-align:left;">
+                     <tr>
+                       <td>Version:</td><td>{}</td>
+                       </tr><tr>
+                       <td>Matlab Path:</td><td>{}</td>
+                       </tr><tr>
+                       <td>Log Level:</td><td>{}</td>                       
+                       </tr><tr>
+                       <td>Log File:</td><td>{}</td>                       
+                     </tr> 
+                 </table><br><br>     
+                </span>
+                <span style="font-family:Georgia; "font-size=30pt;">
+                Distributed under the BSD 3-Clause License<br>
+                <p>Copyright &copy; 2018-2025 ISIS Neutron and Muon Source.</p>
+                All rights reserved
+                </span></i>
+                """
+        label_text = info_template.format(rascal2.RASCAL2_VERSION,matlab_path,log_level,log_file)
+        self._rascal_label.setText(label_text)
