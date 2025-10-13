326d51b04b36144b04bf76c53f96d0cdf0bd9a27
Stephen Nneji <steve.nneji@gmail.com>
2025-10-13 13:52:20 +0100

    ruff fixes

--- rascal2/widgets/controls.py
+++ rascal2/widgets/controls.py
@@ -1,6 +1,7 @@
 """Widget for setting up the Controls class."""
 
-from typing import Any, Callable
+from collections.abc import Callable
+from typing import Any
 
 from pydantic import ValidationError
 from PyQt6 import QtCore, QtWidgets
