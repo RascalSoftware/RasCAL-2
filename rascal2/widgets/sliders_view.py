bdf4c55a1373e529304bb3572f4a547f8bc92480
abuts <Alex.Buts@stfc.ac.uk>
2025-11-03 20:44:13 +0000

    Re #148 Acceptable add_sliders method

--- rascal2/widgets/sliders_view.py
+++ rascal2/widgets/sliders_view.py
@@ -12,22 +12,10 @@ from ratapi.utils.custom_errors import custom_pydantic_validation_error
 from ratapi.utils.enums import Calculations, Geometries, LayerModels
 
 
-from rascal2.widgets.project.project import create_draft_project
-
-from rascal2.widgets.project.lists import ContrastWidget, DataWidget
-from rascal2.widgets.project.tables import (
-    BackgroundsFieldWidget,
-    CustomFileWidget,
-    DomainContrastWidget,
-    LayerFieldWidget,
-    ParameterFieldWidget,
-    ProjectFieldWidget,
-    ResolutionsFieldWidget,
-)
-
 class SlidersViewWidget(QtWidgets.QWidget):
     """
-    The sliders view Widget
+    The sliders view Widget represents properties user intends to fit.
+    The sliders allow user to change the properties and immediately see how the change affects contrast.
     """
 
     def __init__(self, parent):
@@ -40,33 +28,31 @@ class SlidersViewWidget(QtWidgets.QWidget):
                 An instance of the MainWindowView
         """
         super().__init__()
-        self.mdi_holder = None # the variable contains reference to mdi container holding this window
-        self._view_geometry = None # holder for slider view geometry, created to store slider view location
+        self.mdi_holder = None   # the variable contains reference to mdi container holding this widget
+        self._view_geometry = None  # holder for slider view geometry, created to store slider view location
         # within the main window for subsequent calls to show sliders. Not yet restored from hdd properly
         # inherits project geometry on the first view.
-        self._parent = parent
+        self._parent = parent  # reference to main view widget which holds sliders view
 
-        self._create_slider_view_layout()
-        #self._parent_model = self.parent.presenter.model
+        self._prop_to_revert ={}  # dictionary of original properties with fit parameter "true" but have to be restored
+        # back into original project if cancel button is pressed
+        self._prop_to_change = {}  # dictionary of references to properties with fit parameter "true" to build sliders
+        # for and allow changes when slider is moved. Their values are reflected in project and affect plots
 
+        self._sliders = {}   # dictionary of the sliders used to display fittable values
+        # create initial slider view layout and everything else which depends on it
+        self.init()
 
-        #self.parent_model.project_updated.connect(self.update_project_view)
-        #self.parent_model.controls_updated.connect(self.handle_controls_update)
-
-        #project_view = self.create_project_view()
-
-        #self.project_tab.currentChanged.connect(self.edit_project_tab.setCurrentIndex)
-        #self.edit_project_tab.currentChanged.connect(self.project_tab.setCurrentIndex)
 
     def show(self):
         """Overload parent show method to deal with mdi container"""
+        self.init()
         if self.mdi_holder is None:
             self._view_geometry = None
             super().show()
         else:
-            main_layout = self.layout()
-            self._create_sliders_view(main_layout)
-            if self._view_geometry is None: # inherit geometry from project view
+            if self._view_geometry is None:
+                # inherit geometry from project view
                 for window in self._parent.mdi.subWindowList():
                     if window.windowTitle() == "Project":
                         self._view_geometry = window.geometry()
@@ -83,9 +69,63 @@ class SlidersViewWidget(QtWidgets.QWidget):
             self._view_geometry = self.mdi_holder.geometry()
             self.mdi_holder.hide()
 
+    def init(self) -> None:
+        """Initialize common state of the sliders widget
+           extract properties, used to build sliders and generate
+           list of sliders widgets to control these properties
+         """
+        if self.findChild(QtWidgets.QWidget,'AcceptButton') is None:
+            self._create_slider_view_layout()
+
+        proj = self._parent.presenter.model.project
+        update_sliders = self._init_properties_for_sliders(proj)
+        if update_sliders:
+            self._add_sliders_widgets() # call update sliders widget
+        else:
+            self._add_sliders_widgets()
+
+
+    def _init_properties_for_sliders(self,project : ratapi.Project) -> bool:
+        """Take project and copy all properties which have attribute "Fit" == True
+           into dictionary used to build sliders for them. Also set back-up properties
+           dictionary used to reset properties back to their default values if "Cancel"
+           button is pressed.
+
+           Input:
+           ------
+           project: ratapi.Project  -- project to get properties to change
+
+           Returns:
+           --------
+           update_properties -- true if all properties in the project have already
+           had sliders, generated for them so we may update existing widgets instead of generating
+           new ones.
+         """
+        if project is None:
+            return False
+
+        trial_properties = {}
+        n_existing_properties = 0
+        for field_name in ratapi.Project.model_fields:
+            attr = getattr(project, field_name)
+            if isinstance(attr, ratapi.ClassList):
+                param_list = attr.data
+                for prop in param_list:
+                    if isinstance(prop,ratapi.models.Parameter) and prop.fit:
+                        trial_properties[prop.name] = prop
+                        if prop.name in self._prop_to_change:
+                            n_existing_properties += 1
+
+        update_properties = n_existing_properties == len(trial_properties) # if all properties of trial dictionary
+        # are in existing dictionary, we will update widgets instead of adding the new one
+        self._prop_to_change = trial_properties               # References to project properties
+        self._prop_to_revert = deepcopy(self._prop_to_change) # Copy of initial values of project properties
+        return update_properties
 
     def _create_slider_view_layout(self) -> None:
-        """ Create sliders layout with all necessary controls and connections """
+        """ Create sliders layout with all necessary controls and connections
+            but without sliders themselves.
+        """
 
         main_layout = QtWidgets.QVBoxLayout()
         #main_layout.setSpacing(20)
@@ -104,34 +144,47 @@ class SlidersViewWidget(QtWidgets.QWidget):
 
         self.setLayout(main_layout)
 
-    def init(self) -> None:
-        """Initialize state, position and construction of the sliders widget
-        i.e. set up everything except its show/hide state
-         """
+    def _add_sliders_widgets(self) -> None:
+        """ Given sliders view layout and list of properties which can be controlled by sliders
+            add appropriate sliders to sliders view Widget
+        """
+        scroll = self.findChild(QtWidgets.QScrollArea, "Scroll")
+        if scroll is None:
+            main_layout = self.layout()
+            scroll = QtWidgets.QScrollArea()
+            scroll.setWidgetResizable(True)  # important: resize content to fit area
+            scroll.setObjectName("Scroll")
+            main_layout.addWidget(scroll)
+            content = QtWidgets.QWidget()
+            content.setObjectName("Scroll_content")
+            scroll.setWidget(content)
+            # --- Add content layout
+            content_layout = QtWidgets.QVBoxLayout(content)
+        else:
+            content = scroll.findChild(QtWidgets.QWidget, "Scroll_content")
+            content_layout = content.layout()
 
+        # We are adding new sliders, so delete all previous ones
+        for slider in self._sliders.values():
+            slider.deleteLater()
+        self._sliders = {}
 
-    def _create_sliders_view(self,main_layout) -> None:
+        if len(self._prop_to_change) == 0:
+            no_label = QtWidgets.QLabel("No properties to fit, Nothing to view here")
+            no_label.setObjectName("No_properties_to_fit")
+            content_layout.addWidget(no_label)
+            self._sliders['No_properties_to_fit_label'] = no_label
+        else:
+            for prop in self._prop_to_change.values():
+                slider = LabeledSlider(prop)
 
-        scroll = QtWidgets.QScrollArea()
-        scroll.setWidgetResizable(True)  # important: resize content to fit area
-        main_layout.addWidget(scroll)
-        content = QtWidgets.QWidget()
-        scroll.setWidget(content)
-        # --- Add content layout
-        content_layout = QtWidgets.QVBoxLayout(content)
+                self._sliders[prop.name] = slider
+                content_layout.addWidget(slider)
 
-        project = self._parent.presenter.model.project
-        prop_dictionary = create_draft_project(project)
-        for obj in prop_dictionary.values():
-            if isinstance(obj, ratapi.ClassList):
-                for prop in obj:
-                    if hasattr(prop,"fit") and prop.fit:
-                        slider = LabeledSlider(prop)
-                        content_layout.addWidget(slider)
-
-
-    def update_project_view(self, update_tab_index=None) -> None:
-        """Updates the project view."""
+    def _update_sliders_widgets(self, update_tab_index=None) -> None:
+        """Updates the sliders given the project properties to fit are the same
+           but their values may be upgraded
+        """
 
 
     def handle_controls_update(self):
@@ -155,6 +208,12 @@ class SlidersViewWidget(QtWidgets.QWidget):
         """Cancel changes to properties obtained from sliders
         and hide sliders view.
         """
+
+        # as here our properties to change refer directly to project properties
+        # we modify their values directly
+        for key,prop in self._prop_to_revert.items():
+            self._prop_to_change[key].value = prop.value
+
         self._parent.show_or_hide_sliders(False)
 
     def save_changes(self) -> None:
@@ -179,10 +238,11 @@ class SlidersViewWidget(QtWidgets.QWidget):
                 self._parent.show_or_hide_sliders(False)
 
 #=======================================================================================================================
-class LabeledSlider(QtWidgets.QWidget):
+class LabeledSlider(QtWidgets.QFrame):
     def __init__(self, param: ratapi.models.Parameter, parent=None):
         super().__init__(parent)
         self._prop = param  # hold the property controlled by slider
+        self.slider_name = param.name # name the slider as the property it refers to
 
         # Properties of slider widget:
         self._slider_min_idx = 0
@@ -198,7 +258,7 @@ class LabeledSlider(QtWidgets.QWidget):
         # Build all sliders widget and arrange them as expected
         self._slider = self._build_slider(param.value)
 
-        name_label = QtWidgets.QLabel(param.name, alignment=QtCore.Qt.AlignmentFlag.AlignLeft)
+        name_label = QtWidgets.QLabel(self.slider_name, alignment=QtCore.Qt.AlignmentFlag.AlignLeft)
         self._value_label = QtWidgets.QLabel(str(f"{param.value:.3g}"), alignment=QtCore.Qt.AlignmentFlag.AlignRight)
         lab_layout = QtWidgets.QHBoxLayout()
         lab_layout.addWidget(name_label)
@@ -219,6 +279,10 @@ class LabeledSlider(QtWidgets.QWidget):
 
         # signal to update label dynamically
         self._slider.valueChanged.connect(self._update_value)
+
+        self.setObjectName(self.slider_name)
+        self.setFrameShape(QtWidgets.QFrame.Shape.Box)
+        self.setFrameShadow(QtWidgets.QFrame.Shadow.Plain)
 
     def _value_to_slider_pos(self, value: float) -> int:
         """Convert double property value into slider position"""
