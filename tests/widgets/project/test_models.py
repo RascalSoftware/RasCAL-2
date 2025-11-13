4d03d05f20db5b0698768dd7709d09ca5991a27d
abuts <Alex.Buts@stfc.ac.uk>
2025-11-13 17:17:58 +0000

    Re #148 more ruff changes

--- tests/widgets/project/test_models.py
+++ tests/widgets/project/test_models.py
@@ -316,7 +316,8 @@ def test_param_item_delegates_emit_to_slider_subscribers(widget_with_delegates):
     index = widget_with_delegates.model.index(1,1)
     fed = MockEditor()
     n_calls = 0
-    for n_calls,(delegate,field_name) in enumerate(zip(delegates_list,selected_fields)):
+    for n_calls,(delegate,field_name) in enumerate(
+            zip(delegates_list,selected_fields,strict=True)):
         delegate.setModelData(fed,widget_with_delegates.model, index)
         assert sr.call_count == n_calls
         assert sr.cache_state == (index,field_name)
