b211310eef033ff619a4d91ce083ef8f76061e4e
abuts <Alex.Buts@stfc.ac.uk>
2025-11-19 18:48:42 +0000

    Re #162 Remove redundant tests for project/test_project.py

--- tests/widgets/project/test_project.py
+++ tests/widgets/project/test_project.py
@@ -169,8 +169,7 @@ def test_project_widget_initial_state(setup_project_widget):
     assert project_widget.edit_project_tab.currentIndex() == 0
 
 
-@patch("rascal2.ui.view.SlidersViewWidget.hide")
-def test_edit_cancel_button_toggle(mock_hide, setup_project_widget):
+def test_edit_cancel_button_toggle(setup_project_widget):
     """
     Tests clicking the edit button causes the stacked widget to change state.
     """
@@ -179,7 +178,6 @@ def test_edit_cancel_button_toggle(mock_hide, setup_project_widget):
     assert project_widget.stacked_widget.currentIndex() == 0
     project_widget.edit_project_button.click()
     assert project_widget.stacked_widget.currentIndex() == 1
-    assert mock_hide.call_count == 1
 
     assert project_widget.geometry_combobox.currentText() == Geometries.AirSubstrate
     assert project_widget.model_combobox.currentText() == LayerModels.StandardLayers
@@ -193,25 +191,24 @@ def test_edit_cancel_button_toggle(mock_hide, setup_project_widget):
     assert project_widget.calculation_type.text() == Calculations.Normal
 
 
-@patch("rascal2.ui.view.SlidersViewWidget.hide")
-def test_save_changes_to_model_project(mock_hide, setup_project_widget):
+def test_save_changes_to_model_project(setup_project_widget):
     """
     Tests that making changes to the project settings
     """
+    project_widget = setup_project_widget
 
-    setup_project_widget.edit_project_button.click()
-    assert mock_hide.call_count == 1
+    project_widget.edit_project_button.click()
 
-    setup_project_widget.calculation_combobox.setCurrentText(Calculations.Domains)
-    setup_project_widget.geometry_combobox.setCurrentText(Geometries.SubstrateLiquid)
-    setup_project_widget.model_combobox.setCurrentText(LayerModels.CustomXY)
+    project_widget.calculation_combobox.setCurrentText(Calculations.Domains)
+    project_widget.geometry_combobox.setCurrentText(Geometries.SubstrateLiquid)
+    project_widget.model_combobox.setCurrentText(LayerModels.CustomXY)
 
-    assert setup_project_widget.draft_project["geometry"] == Geometries.SubstrateLiquid
-    assert setup_project_widget.draft_project["model"] == LayerModels.CustomXY
-    assert setup_project_widget.draft_project["calculation"] == Calculations.Domains
+    assert project_widget.draft_project["geometry"] == Geometries.SubstrateLiquid
+    assert project_widget.draft_project["model"] == LayerModels.CustomXY
+    assert project_widget.draft_project["calculation"] == Calculations.Domains
 
-    setup_project_widget.save_changes()
-    assert setup_project_widget.parent.presenter.edit_project.call_count == 1
+    project_widget.save_changes()
+    assert project_widget.parent.presenter.edit_project.call_count == 1
 
 
 def test_cancel_changes_to_model_project(setup_project_widget):
