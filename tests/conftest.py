90f09a5d37ea8fb5e20845a7ebcf9a53091df5fb
Mike Sullivan <michael.sullivan@stfc.ac.uk>
2026-08-20 16:28:40 +0100

    pytests differs between system and unit tests using flags --run-system-tests and --run-unit-tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 import tempfile
 from pathlib import Path
 from unittest.mock import patch
@@ -54,3 +55,39 @@ def mock_setting(request):
             target.stop()
 
     request.addfinalizer(teardown_mock_setting)
+
+
+def pytest_addoption(parser):
+    parser.addoption("--run-system-tests", action="store_true", default=False, help="Run GUI system tests offscreen")
+    parser.addoption("--run-system-tests-show", action="store_true", default=False, help="Run GUI system tests")
+    parser.addoption("--run-unit-tests", action="store_true", default=False, help="Run unit tests")
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "system: GUI system tests")
+    config.addinivalue_line("markers", "unit: unit tests")
+
+
+allowed_markers = []
+skipped_tests = []
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--run-system-tests"):
+        allowed_markers.append(pytest.mark.system.mark)
+        os.environ["QT_QPA_PLATFORM"] = "minimal"
+    if config.getoption("--run-system-tests-show"):
+        allowed_markers.append(pytest.mark.system.mark)
+        os.environ["QT_QPA_PLATFORM"] = ""
+    if config.getoption("--run-unit-tests") or len(allowed_markers) == 0:
+        allowed_markers.append(pytest.mark.unit.mark)
+    for item in items:
+        if "gui_system" in item.nodeid:
+            item.add_marker(pytest.mark.system)
+        else:
+            item.add_marker(pytest.mark.unit)
+        if any(mark in allowed_markers for mark in item.own_markers):
+            pass
+        else:
+            item.add_marker(pytest.mark.skip(reason="Test not selected"))
+            skipped_tests.append(item.nodeid)
