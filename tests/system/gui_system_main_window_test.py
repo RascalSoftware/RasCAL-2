f94108b56000f2db99125f8e74ea0f116ed123a6
Mike Sullivan <michael.sullivan@stfc.ac.uk>
2026-08-21 12:55:19 +0100

    added TestGuiSystemMainWindow test class

---
+++ tests/system/gui_system_main_window_test.py
@@ -0,0 +1,15 @@
+from tests.system.gui_system_base import GuiSystemBase
+
+
+class TestGuiSystemMainWindow(GuiSystemBase):
+
+    def setUp(self) -> None:
+        super().setUp()
+
+    def tearDown(self) -> None:
+        super().tearDown()
+
+    def test_main_window(self):
+        self.main_window.presenter.create_project("project", ".")
+        names = [win.windowTitle() for win in self.main_window.mdi.subWindowList()]
+        assert names == ["Fitting Controls", "Terminal", "Project", "Plots"]
