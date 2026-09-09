705ff87f71321e8dd531f9c9b85e86aaf4657053
Mike Sullivan <michael.sullivan@stfc.ac.uk>
2026-09-09 13:21:09 +0100

    unit and system tests run by default, can skip tests with --skip_system_tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,3 @@
-import os
 import tempfile
 from pathlib import Path
 from unittest.mock import patch
@@ -51,9 +50,7 @@ def mock_setting(request):
 
 
 def pytest_addoption(parser):
-    parser.addoption("--run-system-tests", action="store_true", default=False, help="Run GUI system tests offscreen")
-    parser.addoption("--run-system-tests-show", action="store_true", default=False, help="Run GUI system tests")
-    parser.addoption("--run-unit-tests", action="store_true", default=False, help="Run unit tests")
+    parser.addoption("--skip_system_tests", action="store_true", default=False, help="Skip GUI system tests")
 
 
 def pytest_configure(config):
@@ -66,14 +63,11 @@ skipped_tests = []
 
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--run-system-tests"):
-        allowed_markers.append(pytest.mark.system.mark)
-        os.environ["QT_QPA_PLATFORM"] = "minimal"
-    if config.getoption("--run-system-tests-show"):
-        allowed_markers.append(pytest.mark.system.mark)
-        os.environ["QT_QPA_PLATFORM"] = ""
-    if config.getoption("--run-unit-tests") or len(allowed_markers) == 0:
+    if config.getoption("--skip_system_tests"):
         allowed_markers.append(pytest.mark.unit.mark)
+    if len(allowed_markers) == 0:
+        allowed_markers.append(pytest.mark.unit.mark)
+        allowed_markers.append(pytest.mark.system.mark)
     for item in items:
         if "gui_system" in item.nodeid:
             item.add_marker(pytest.mark.system)
