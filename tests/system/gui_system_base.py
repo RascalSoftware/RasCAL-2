9563d1e0eaf79550761d4efb1098ddf9d7fa3726
Mike Sullivan <michael.sullivan@stfc.ac.uk>
2026-09-10 13:53:14 +0100

    ruff fix

--- tests/system/gui_system_base.py
+++ tests/system/gui_system_base.py
@@ -44,8 +44,8 @@ class GuiSystemBase(unittest.TestCase):
         if not self.no_exceptions:
             raise Exception("An exception occurred in a PyQt slot")
         sys.excepthook = sys.__excepthook__
-        LOGGER = logging.getLogger("rascal2")
-        LOGGER.handlers.clear()
+        logger = logging.getLogger("rascal2")
+        logger.handlers.clear()
         QTimer.singleShot(SHORT_DELAY, lambda: self._click_messagebox("Discard"))
         self.main_window.close()
         wait_until(
