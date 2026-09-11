57806fc2e90839cbbff16c66dac53e1ce3facafe
Mike Sullivan <michael.sullivan@stfc.ac.uk>
2026-09-11 13:52:22 +0100

    test_run now runs each procedure and compares against expected values

--- tests/system/gui_system_loading_test.py
+++ tests/system/gui_system_loading_test.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from PyQt6.QtTest import QTest
 from PyQt6.QtWidgets import QApplication
+from parameterized import parameterized
 
 from rascal2.dialogs.startup_dialog import LoadDialog
 from tests.system.gui_system_base import SHORT_DELAY, GuiSystemBase, wait_until
@@ -25,7 +26,14 @@ class TestGuiSystemLoading(GuiSystemBase):
         QTest.qWait(SHORT_DELAY)
         assert self.main_window.presenter.model.project.name == "DSPC Standard Layers"
 
-    def test_run(self):
+    @parameterized.expand([
+        ("calculate", 142.346, 1000),
+        ("simplex", 13.1816, 1000),
+        ("de", 10.4746, 1000),
+        ("ns", [9, 11], 4000),
+        ("dream", 9.67234, 1000),
+    ])
+    def test_run(self, procedure_name, expected_chi, test_duration):
         QTest.qWait(SHORT_DELAY)
         self.main_window.startup_dlg.import_project_button.click()
         load_dialog = self.main_window.findChild(LoadDialog)
@@ -33,9 +41,13 @@ class TestGuiSystemLoading(GuiSystemBase):
         load_dialog.example_list_widget.itemClicked.emit(load_dialog.example_list_widget.item(0))
         wait_until(lambda: self.main_window.controls_widget.chi_squared.text() != "")
         self.main_window.controls_widget.update_chi_squared("")
+        self.main_window.controls_widget.procedure_dropdown.setCurrentText(procedure_name)
         QApplication.processEvents()
         self.main_window.controls_widget.run_button.click()
-        wait_until(lambda: "Finished RAT" in self.main_window.terminal_widget.text_area.toPlainText())
+        wait_until(lambda: "Finished RAT" in self.main_window.terminal_widget.text_area.toPlainText(), max_retry=test_duration)
         QTest.qWait(SHORT_DELAY)
-        assert self.main_window.controls_widget.chi_squared.text() == "142.346"
+        if isinstance(expected_chi, list):
+            assert expected_chi[0] <= float(self.main_window.controls_widget.chi_squared.text()) <= expected_chi[1]
+        else:
+            assert self.main_window.controls_widget.chi_squared.text() == str(expected_chi)
         assert self.main_window.presenter.runner.error is None
